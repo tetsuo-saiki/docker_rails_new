df1a758e5e2f2220e23e3abc9c9c940e1ce8862f
tetsuo saiki <paso-ichio.mlkre@hotmail.co.jp>
2018-11-10 16:50:21 +0900

    ナビバーとフラッシュメッセージ追加

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,8 @@ Rails.application.routes.draw do
   #   :sessions => 'users/sessions'   
   # }
 
-  # devise_scope :user do
-  #   get "sign_in", :to => "users/sessions#new"
-  #   get "sign_out", :to => "users/sessions#destroy"
-  # end
+  devise_scope :user do
+    get "sign_in", :to => "users/sessions#new"
+    get "sign_out", :to => "users/sessions#destroy"
+  end
 end
